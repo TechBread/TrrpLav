d8ed3d313390915741b37545082c417e9b78ec16
efalkovich <efalkovich9@gmail.com>
2023-03-31 10:05:33 +0500

    hujkDADASDF

---
+++ Front-end/GameData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Front_end
+{
+    internal class GameData
+    {
+        public int moveCount { get; set; }
+        public int lifeLeft { get; set; }
+        public GameData(int moveCount, int lifeLeft)
+        {
+            this.moveCount = moveCount;
+            this.lifeLeft = lifeLeft;
+        }   
+    }
+}
